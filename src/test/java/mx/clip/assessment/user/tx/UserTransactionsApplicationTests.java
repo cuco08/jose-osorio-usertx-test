e19b6e2d81f2dfd619d45f0e909511b5cbb0cac7
Jose Osorio (Cuco) <C-Jose.RefugioOsorioSoto1@charter.com>
2020-10-18 00:18:20 -0500

    Added initial spring-boot project.

---
+++ src/test/java/mx/clip/assessment/user/tx/UserTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.clip.assessment.user.tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
