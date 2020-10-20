c6df76db4354cc10ecf5bd19a15b77a994dfd882
Jose Osorio (Cuco) <C-Jose.RefugioOsorioSoto1@charter.com>
2020-10-19 23:13:51 -0500

    Added integration tests.

--- service/src/test/java/mx/clip/assessment/user/tx/service/UserTransactionServiceTest.java
+++ service/src/test/java/mx/clip/assessment/user/tx/service/UserTransactionServiceTest.java
@@ -25,6 +25,7 @@ import java.util.UUID;
 import static mx.clip.assessment.user.tx.service.util.ServiceTestData.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
